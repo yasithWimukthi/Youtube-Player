c2668da3e1b004320d17be7a08dc9d5108bd1b2f
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-20 16:40:02 +0530

    Edit MainActivity

--- app/src/main/java/com/androidstudio/java/youtubeplayer/MainActivity.java
+++ app/src/main/java/com/androidstudio/java/youtubeplayer/MainActivity.java
@@ -2,13 +2,42 @@ package com.androidstudio.java.youtubeplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSingle = (Button) findViewById(R.id.btnPlaySingle);
+        Button btnStandalone = (Button) findViewById(R.id.btnStandalone);
+
+        btnSingle.setOnClickListener(this);
+        btnStandalone.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+
+        switch(v.getId()){
+            case R.id.btnPlaySingle :
+                intent = new Intent(this, YoutubeActivity.class);
+                break;
+            case R.id.btnStandalone:
+                intent = new Intent(this,StandaloneActivity.class);
+                break;
+            default:
+
+        }
+
+        if(intent != null){
+            startActivity(intent);
+        }
     }
 }
